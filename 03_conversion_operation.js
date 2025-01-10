15723e91b09b893b2ebe30bc19ecf31ac563c65e
Diwakar Choudhary <jaiswaldiwakar91@gmail.com>
2025-01-10 13:39:12 +0530

    operation & conversion

--- 03_conversion_operation.js
+++ 03_conversion_operation.js
@@ -18,3 +18,52 @@ console.log(booleanIsLoggedIn);;
 // 1 => true
 // " " => false
 // "hello" => true
+
+
+// ****************OPERATION****************
+let value =3;
+let negValue = -value;
+console.log(negValue);
+
+console.log(2**3); // 2 raise to the power 3
+
+
+let str1 = "diwakar"
+let str2 = " choudhary"
+
+let str3 = str1+str2;
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1" + 2 + 2 );
+console.log(1+1+ "2" + 3 + "4");
+
+
+console.log(+true);
+console.log(+"");
+
+
+let num = 100;
+ 
+let num2 = ++num;
+console.log(num2);
+console.log(num);
+console.log(num2);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+
+
+
+
+
+
+
+
+
+
+
+
